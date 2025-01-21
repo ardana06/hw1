1bd95797356171d87cb444f5b05c855801d27ed4
Ardana <fhggffhf@gmail.com>
2025-01-21 19:53:13 +0500

    gradus

---
+++ convert-temperatures.js
@@ -0,0 +1,13 @@
+const gradus = function(Celsia){ 
+    let sum = (Celsia * 9 / 5 + 32); 
+    console.log("Фаренгейт равен = " + sum); 
+} 
+gradus(25) 
+ 
+ 
+ 
+const Celsia = function(Farengeit){ 
+    let sum = (Farengeit - 32 * 5 / 9); 
+    console.log("Цельсия равен = " + sum); 
+} 
+Celsia(77)
