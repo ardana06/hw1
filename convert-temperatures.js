5c896949f65d478bc652c5bbde6287a187081a4c
Ardana <fhggffhf@gmail.com>
2025-01-22 23:09:27 +0500

    deleted hw1

--- convert-temperatures.js
+++ convert-temperatures.js
@@ -1,22 +1,3 @@
-const Farengeit = function(Celsia){  
-    let sum = Celsia * 9 / 5 + 32;  
-    console.log("Фаренгейт равен = " + sum);  
-}  
-Farengeit(10)  
-Farengeit(55)  
-Farengeit(29)  
-  
-  
-const Celsia = function(Farengeit){  
-    let sum = (Farengeit - 32) * 5 / 9;  
-    console.log("Цельсия равен = " + sum);  
-}  
-Celsia(50) 
-Celsia(131) 
-Celsia(84.2)
-
-
-
 //дз - 2 block-scopes
 
 let globalVariable = "Я глобал"; //работает везде 
