70465ad5060a502ead8cbe0654c2a77a32e0359a
Ardana <fhggffhf@gmail.com>
2025-01-22 19:46:36 +0500

    change name

--- convert-temperatures.js
+++ convert-temperatures.js
@@ -1,13 +1,17 @@
-const gradus = function(Celsia){ 
-    let sum = (Celsia * 9 / 5 + 32); 
-    console.log("Фаренгейт равен = " + sum); 
-} 
-gradus(25) 
- 
- 
- 
-const Celsia = function(Farengeit){ 
-    let sum = (Farengeit - 32 * 5 / 9); 
-    console.log("Цельсия равен = " + sum); 
-} 
-Celsia(77)
+const gradus = function(Celsia){  
+    let sum = Celsia * 9 / 5 + 32;  
+    console.log("Фаренгейт равен = " + sum);  
+}  
+gradus(10)  
+gradus(55)  
+gradus(29)  
+  
+  
+  
+const Celsia = function(Farengeit){  
+    let sum = (Farengeit - 32) * 5 / 9;  
+    console.log("Цельсия равен = " + sum);  
+}  
+Celsia(50) 
+Celsia(131) 
+Celsia(84.2)
