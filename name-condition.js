3cfcf03c63d99f2f9c215f6d7bfe1b0f4729a157
Ardana <fhggffhf@gmail.com>
2025-01-25 23:17:15 +0500

    add hw1

---
+++ name-condition.js
@@ -0,0 +1,16 @@
+let chooseName = function(name) {
+
+    if (name == "Zhasmine") {
+        console.log("Сегодня твой счастливый день! получи свой приз.");
+    } else if(name == "Aidana") {
+        console.log("Сегодня и твой счастливый день! получи свою скидку.");
+    } else {
+        console.log ("Добро пожаловать, " + name); 
+    }
+    }
+    
+    chooseName("Саша");
+    chooseName("Zhasmine");
+    chooseName("Aidana");   
+    chooseName("Катя");
+    chooseName("Банан");
