7c42cf1a9e0fc3cec64d916f0acb85c9d1368e64
Ardana <fhggffhf@gmail.com>
2025-01-26 00:48:14 +0500

    add

--- convert-temperatures.js
+++ convert-temperatures.js
@@ -1,17 +1,17 @@
 //дз 1 - convert-temperatures
 
 const Farengeit = function(Celsia){  
-    let sum = Celsia * 9 / 5 + 32;  
-    console.log("Фаренгейт равен = " + sum);  
+  let sum = Celsia * 9 / 5 + 32;  
+  console.log("Фаренгейт равен = " + sum);  
 }  
 Farengeit(10)  
 Farengeit(55)  
 Farengeit(29)  
-  
-  
+
+
 const Celsia = function(Farengeit){  
-    let sum = (Farengeit - 32) * 5 / 9;  
-    console.log("Цельсия равен = " + sum);  
+  let sum = (Farengeit - 32) * 5 / 9;  
+  console.log("Цельсия равен = " + sum);  
 }  
 Celsia(50) 
 Celsia(131) 
