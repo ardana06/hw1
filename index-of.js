a384689234983acd51548199ae3a46a2f3f481d0
Ardana <fhggffhf@gmail.com>
2025-03-09 15:47:38 +0500

    add hw

---
+++ index-of.js
@@ -0,0 +1,8 @@
+// indexOf() metods
+
+const paragrah = "the quiek brown fox jumps over the lazy dog. If the dog barked , was a really lazy?"
+const serchTerm ="dog";
+
+let ind=paragrah.indexOf(serchTerm);
+
+console.log(paragrah.lastIndexOf(serchTerm, ind+1)); //40
